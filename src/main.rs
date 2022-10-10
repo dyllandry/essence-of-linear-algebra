a5e7eda13786925b4ba5adc2ef775458d7f120ff
Dylan Landry <dylanglandry@gmail.com>
2022-10-10 12:34:43 -0400

    remove "d" from type names
    Wasn't really a 3Vector, just a vector with 3 elements. And wasn't
    really a 3D matrix, just a 2D matrix with 3 rows and 3 columns.

--- src/main.rs
+++ src/main.rs
@@ -4,62 +4,62 @@ fn main() {
 
 // video: https://youtu.be/kYB8IZa5AuE?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=364
 #[derive(Copy, Clone, PartialEq, Debug)]
-struct Matrix2D {
-    v1: Vector2D, // [ v1.x, v2.x ]
-    v2: Vector2D, // [ v1.y, v2.y ]
+struct Matrix2 {
+    col1: Vector2,
+    col2: Vector2,
 }
 
 // video: https://youtu.be/XkY2DOUCWMU?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=119
-impl std::ops::Mul for Matrix2D {
-    type Output = Matrix2D;
+impl std::ops::Mul for Matrix2 {
+    type Output = Matrix2;
     fn mul(self, rhs: Self) -> Self::Output {
         let left_m = self;
         let right_m = rhs;
 
-        return Matrix2D {
-            v1: right_m.v1.transform(left_m),
-            v2: right_m.v2.transform(left_m),
+        return Matrix2 {
+            col1: right_m.col1.transform(left_m),
+            col2: right_m.col2.transform(left_m),
         };
     }
 }
 
 // video: https://www.youtube.com/watch?v=fNk_zzaMoSs&list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&index=2
 #[derive(PartialEq, Debug, Copy, Clone)]
-struct Vector2D {
+struct Vector2 {
     x: f32,
     y: f32,
 }
 
-impl Vector2D {
+impl Vector2 {
     // video: https://youtu.be/fNk_zzaMoSs?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=414
-    fn scale(&self, scalar: f32) -> Vector2D {
-        return Vector2D {
+    fn scale(&self, scalar: f32) -> Vector2 {
+        return Vector2 {
             x: scalar * self.x,
             y: scalar * self.y,
         };
     }
 
     // video: https://youtu.be/kYB8IZa5AuE?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=210
-    fn transform(&self, m: Matrix2D) -> Vector2D {
-        return m.v1.scale(self.x) + m.v2.scale(self.y);
+    fn transform(&self, m: Matrix2) -> Vector2 {
+        return m.col1.scale(self.x) + m.col2.scale(self.y);
     }
 }
 
 // video: https://youtu.be/fNk_zzaMoSs?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=276
-impl std::ops::Add for Vector2D {
-    type Output = Vector2D;
+impl std::ops::Add for Vector2 {
+    type Output = Vector2;
     fn add(self, rhs: Self) -> Self::Output {
-        return Vector2D {
+        return Vector2 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
         };
     }
 }
 
-impl std::ops::Sub for Vector2D {
-    type Output = Vector2D;
+impl std::ops::Sub for Vector2 {
+    type Output = Vector2;
     fn sub(self, rhs: Self) -> Self::Output {
-        return Vector2D {
+        return Vector2 {
             x: self.x - rhs.x,
             y: self.y - rhs.y,
         };
@@ -68,16 +68,16 @@ impl std::ops::Sub for Vector2D {
 
 // video: https://youtu.be/fNk_zzaMoSs?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=243
 #[derive(PartialEq, Debug)]
-struct Vector3D {
+struct Vector3 {
     x: f32,
     y: f32,
     z: f32,
 }
 
-impl Vector3D {
+impl Vector3 {
     // video: https://youtu.be/fNk_zzaMoSs?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=414
-    fn scale(&self, scalar: f32) -> Vector3D {
-        return Vector3D {
+    fn scale(&self, scalar: f32) -> Vector3 {
+        return Vector3 {
             x: scalar * self.x,
             y: scalar * self.y,
             z: scalar * self.z,
@@ -86,10 +86,10 @@ impl Vector3D {
 }
 
 // video: https://youtu.be/fNk_zzaMoSs?list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B&t=276
-impl std::ops::Add for Vector3D {
-    type Output = Vector3D;
+impl std::ops::Add for Vector3 {
+    type Output = Vector3;
     fn add(self, rhs: Self) -> Self::Output {
-        return Vector3D {
+        return Vector3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
@@ -97,10 +97,10 @@ impl std::ops::Add for Vector3D {
     }
 }
 
-impl std::ops::Sub for Vector3D {
-    type Output = Vector3D;
+impl std::ops::Sub for Vector3 {
+    type Output = Vector3;
     fn sub(self, rhs: Self) -> Self::Output {
-        return Vector3D {
+        return Vector3 {
             x: self.x - rhs.x,
             y: self.y - rhs.y,
             z: self.z - rhs.z,
@@ -112,65 +112,66 @@ impl std::ops::Sub for Vector3D {
 mod tests {
     use crate::*;
 
-    mod vector_2d {
+    mod vector2 {
         use crate::*;
+
         #[test]
-        fn can_add_two_2d_vectors() {
-            let v1 = Vector2D { x: 1.0, y: 2.0 };
-            let v2 = Vector2D { x: 2.0, y: 3.0 };
+        fn can_add_two_vector2() {
+            let v1 = Vector2 { x: 1.0, y: 2.0 };
+            let v2 = Vector2 { x: 2.0, y: 3.0 };
             let result = v1 + v2;
-            let expected = Vector2D { x: 3.0, y: 5.0 };
+            let expected = Vector2 { x: 3.0, y: 5.0 };
             assert_eq!(result, expected);
         }
 
         #[test]
-        fn can_subtract_two_2d_vectors() {
-            let v1 = Vector2D { x: 2.0, y: 3.0 };
-            let v2 = Vector2D { x: 1.0, y: 1.0 };
+        fn can_subtract_two_vector2() {
+            let v1 = Vector2 { x: 2.0, y: 3.0 };
+            let v2 = Vector2 { x: 1.0, y: 1.0 };
             let result = v1 - v2;
-            let expected = Vector2D { x: 1.0, y: 2.0 };
+            let expected = Vector2 { x: 1.0, y: 2.0 };
             assert_eq!(result, expected);
         }
 
         #[test]
-        fn can_scale_a_vector_2d() {
-            let v = Vector2D { x: 1.0, y: 2.0 };
+        fn can_scale_a_vector2() {
+            let v = Vector2 { x: 1.0, y: 2.0 };
             let scalar = 3.0;
-            let expected = Vector2D { x: 3.0, y: 6.0 };
+            let expected = Vector2 { x: 3.0, y: 6.0 };
             let result = v.scale(scalar);
             assert_eq!(result, expected);
         }
 
         #[test]
-        fn can_transform_a_vector_2d() {
-            let v = Vector2D { x: 1.0, y: 1.0 };
-            let rotate_180 = Matrix2D {
-                v1: Vector2D { x: -1.0, y: 0.0 },
-                v2: Vector2D { x: 0.0, y: -1.0 },
+        fn can_transform_a_vector2() {
+            let v = Vector2 { x: 1.0, y: 1.0 };
+            let rotate_180 = Matrix2 {
+                col1: Vector2 { x: -1.0, y: 0.0 },
+                col2: Vector2 { x: 0.0, y: -1.0 },
             };
-            let expected = Vector2D { x: -1.0, y: -1.0 };
+            let expected = Vector2 { x: -1.0, y: -1.0 };
             let result = v.transform(rotate_180);
             assert_eq!(result, expected);
         }
     }
 
-    mod vector_3d {
+    mod vector3 {
         use crate::*;
 
         #[test]
-        fn can_add_two_3d_vectors() {
-            let v1 = Vector3D {
+        fn can_add_two_vector3() {
+            let v1 = Vector3 {
                 x: 1.0,
                 y: 2.0,
                 z: 3.0,
             };
-            let v2 = Vector3D {
+            let v2 = Vector3 {
                 x: 2.0,
                 y: 3.0,
                 z: 6.0,
             };
             let result = v1 + v2;
-            let expected = Vector3D {
+            let expected = Vector3 {
                 x: 3.0,
                 y: 5.0,
                 z: 9.0,
@@ -179,19 +180,19 @@ mod tests {
         }
 
         #[test]
-        fn can_subtract_two_3d_vectors() {
-            let v1 = Vector3D {
+        fn can_subtract_two_vector3() {
+            let v1 = Vector3 {
                 x: 2.0,
                 y: 5.0,
                 z: 7.0,
             };
-            let v2 = Vector3D {
+            let v2 = Vector3 {
                 x: 1.0,
                 y: 2.0,
                 z: 3.0,
             };
             let result = v1 - v2;
-            let expected = Vector3D {
+            let expected = Vector3 {
                 x: 1.0,
                 y: 3.0,
                 z: 4.0,
@@ -200,14 +201,14 @@ mod tests {
         }
 
         #[test]
-        fn can_scale_a_vector_3d() {
-            let v = Vector3D {
+        fn can_scale_a_vector3() {
+            let v = Vector3 {
                 x: 1.0,
                 y: 2.0,
                 z: 3.0,
             };
             let scalar = 3.0;
-            let expected = Vector3D {
+            let expected = Vector3 {
                 x: 3.0,
                 y: 6.0,
                 z: 9.0,
@@ -217,23 +218,23 @@ mod tests {
         }
     }
 
-    mod matrix_2d {
+    mod matrix2 {
         use crate::*;
 
         #[test]
-        fn can_multiply_two_2d_matrices() {
-            let rotate_90 = Matrix2D {
-                v1: Vector2D { x: 0.0, y: -1.0 },
-                v2: Vector2D { x: 1.0, y: 0.0 },
+        fn can_multiply_two_matrix2() {
+            let rotate_90 = Matrix2 {
+                col1: Vector2 { x: 0.0, y: -1.0 },
+                col2: Vector2 { x: 1.0, y: 0.0 },
             };
-            let scale_x_by_2 = Matrix2D {
-                v1: Vector2D { x: 2.0, y: 0.0 },
-                v2: Vector2D { x: 0.0, y: 1.0 },
+            let scale_x_by_2 = Matrix2 {
+                col1: Vector2 { x: 2.0, y: 0.0 },
+                col2: Vector2 { x: 0.0, y: 1.0 },
             };
             // expected result of rotating then scaling
-            let expected = Matrix2D {
-                v1: Vector2D { x: 0.0, y: -1.0 },
-                v2: Vector2D { x: 2.0, y: 0.0 },
+            let expected = Matrix2 {
+                col1: Vector2 { x: 0.0, y: -1.0 },
+                col2: Vector2 { x: 2.0, y: 0.0 },
             };
             let result = scale_x_by_2 * rotate_90;
             assert_eq!(result, expected);
